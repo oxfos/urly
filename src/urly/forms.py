3169d2489eded2c03047123f5a2fe13aacb60cf5
oxfos <rinaldo_bertossa@hotmail.com>
2020-10-01 12:24:17 +0200

    Updated Shortcode model and form.

---
+++ src/urly/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Shortcode
+
+
+class ShortcodeForm(forms.ModelForm):
+    """ModelForm to handle Shortcode data."""
+    class Meta:
+        model = Shortcode
+        fields = ['url', 'scode']
+
--- src/urly/models.py
+++ src/urly/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class Shortcode(models.Model):
     """Class representing a shortcode model."""
-    shortcode = models.CharField(max_length=6, blank=True)
+    scode = models.CharField(max_length=6, blank=True)
     url = models.URLField()
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.scode
