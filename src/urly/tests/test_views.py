ea95854c7a61525e8d7c54e160f981617e17ba49
oxfos <rinaldo_bertossa@hotmail.com>
2020-10-02 20:33:59 +0200

    Deleted error handling through redirects and html.

---
+++ src/urly/tests/test_views.py
@@ -0,0 +1,65 @@
+from django.test import TestCase
+from urly.models import Shortcode
+
+
+
+class Test_homepage_view(TestCase):
+    """Test class for homepage view."""
+
+    def test_1_get_page(self):
+        # Test GET works as expected.
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'urly/index.html') 
+
+
+class Test_getshortcode_view(TestCase):
+    """Test class for get_shortcode view."""
+
+    def setUp(self):
+        Shortcode.objects.create(url='https://www.google.com', shortcode='catch_')
+
+    def test_2_inexistent_url(self):
+        # Test valid but non-existent url.
+        response = self.client.post('/shorten', {'url':'http://www.79as.com/'})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.reason_phrase, 'Url not present')
+
+    def test_shortcode_is_not_unique(self):
+        # Test shortcode already in use.
+        response = self.client.post('/shorten', {'url':'https://www.google.com', 'shortcode': 'catch_'})
+        self.assertEqual(response.status_code, 409)
+        self.assertEqual(response.reason_phrase, 'Shortcode already in use')
+
+    def test_shortcode_is_invalid(self):
+        # Test invalid shortcode.
+        response = self.client.post('/shorten', {'url':'https://www.google.com', 'shortcode':'catch$'})
+        self.assertEqual(response.status_code, 412)
+        self.assertEqual(response.reason_phrase, 'The provided shortcode is invalid')
+
+    def test_existent_url(self):
+        # Test valid and existent url.
+        response = self.client.post('/shorten', {'url': 'https://www.google.com'})
+        self.assertEqual(response.status_code, 201)
+        self.assertContains(response, 'shortcode', status_code=201)
+
+
+
+class Test_check_shortcode_view(TestCase):
+    """Test class for check_shortcode view."""
+
+    def setUp(self):
+        # Create a db Shortcode object.
+        self.entry = Shortcode.objects.create(url="https://www.google.com", shortcode="google")
+
+    def test_2_shortcode_exists(self):
+        # Tests an existing shortcode...
+        response = self.client.get('/google')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, self.entry.url, fetch_redirect_response=False)
+        self.assertEqual(response['Location'], self.entry.url)
+
+    def test_3_shortcode_does_not_exist(self):
+        # Tests an inexistent shortcode...
+        response = self.client.get('/l3skja', follow=True)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.reason_phrase, 'Shortcode not found')
--- src/urly/urls.py
+++ src/urly/urls.py
@@ -6,10 +6,6 @@ app_name = 'urly'
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('shorten/', views.get_shortcode, name='shorten'),
-    path('400/', views.error_400, name='error_400'),
-    path('404/', views.error_404, name='error_404'),
-    path('409/', views.error_409, name='error_409'),
-    path('412/', views.error_412, name='error_412'),
-    path('<str:shortcode_2>/', views.check_shortcode, name='check_shortcode'),
+    path('shorten', views.get_shortcode, name='shorten'),
+    path('<str:shortcode_2>', views.check_shortcode, name='check_shortcode'),
 ]
