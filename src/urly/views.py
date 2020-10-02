f454b977e40d8777f1e33bde18aa91549900604b
oxfos <rinaldo_bertossa@hotmail.com>
2020-10-02 20:36:14 +0200

    Changed /shorten response from redirect/render to HttpResponse.

--- src/urly/views.py
+++ src/urly/views.py
@@ -1,6 +1,6 @@
 
 import requests
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, redirect
 from django.template import RequestContext
 from .forms import ShortcodeForm
@@ -16,20 +16,31 @@ def homepage(request):
 
 def get_shortcode(request):
     """View to shorten url."""
-    if request.method == 'POST':
-        form = ShortcodeForm(data=request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            entry = form.save(commit=False)
-            if url_exists(cd['url']) != True:
-                return redirect('urly:error_400')
-            elif is_not_unique(cd['shortcode']):
-                return redirect('urly:error_409')
-            elif is_invalid(cd['shortcode']):
-                return redirect('urly:error_412')
-            elif cd['shortcode'] == '':
-                entry.shortcode = get_unique_shortcode(6)
-            entry.save()
+    form = ShortcodeForm(data=request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        entry = form.save(commit=False)
+        if url_exists(cd['url']) != True:
+            response = HttpResponse('Url not present.<br><a href="/">Try again</a>.')
+            response.reason_phrase = 'Url not present'
+            response.status_code = 400
+            return response
+        elif is_not_unique(cd['shortcode']):
+            response = HttpResponse('Shortcode already in use.<br><a href="/">Try again</a>.')
+            response.reason_phrase = 'Shortcode already in use'
+            response.status_code = 409
+            return response
+        elif is_invalid(cd['shortcode']):
+            response = HttpResponse('The provided shortcode is invalid.<br><a href="/">Try again</a>.')
+            response.reason_phrase = 'The provided shortcode is invalid'
+            response.status_code = 412
+            return response
+        elif cd['shortcode'] == '':
+            entry.shortcode = get_unique_shortcode(6)
+        entry.save()
+        response = HttpResponse('{"shortcode":"%s"}' % (entry.shortcode))
+        response.status_code = 201
+        return response
     return render(request, 'urly/index.html', {'form':form})
 
 
@@ -38,37 +49,13 @@ def check_shortcode(request, shortcode_2):
     try:
         shortcode = Shortcode.objects.get(shortcode=shortcode_2)
     except:
-        return redirect('urly:error_404')
+        response = HttpResponse('Shortcode not found')
+        response.status_code = 404
+        response.reason_phrase = 'Shortcode not found'
+        return response
     else:
-        return redirect(shortcode.url)
+        response = HttpResponseRedirect(shortcode.url)
+        response.status_code = 302
+        response['Location'] = shortcode.url
+        return response
         
-
-
-# ERROR VIEWS
-
-def error_400(request, exception=None):
-    response = render(request, 'urly/errors/400.html')
-    response.reason_phrase = 'Url not present'
-    response.status_code = 400
-    return response
-
-
-def error_404(request, exception=None):
-    response = render(request, 'urly/errors/404.html')
-    response.reason_phrase = 'Shortcode not found'
-    response.status_code = 404
-    return response
-
-
-def error_409(request, exception=None):
-    response = render(request, 'urly/errors/409.html')
-    response.reason_phrase = 'Shortcode already in use'
-    response.status_code = 409
-    return response
-
-
-def error_412(request, exception=None):
-    response = render(request, 'urly/errors/412.html')
-    response.reason_phrase = 'The provided shortcode is invalid'
-    response.status_code = 412
-    return response
