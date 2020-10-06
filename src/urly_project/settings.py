7e562b2cc954f3b6ec77422ee99135c931d6f8cf
oxfos <rinaldo_bertossa@hotmail.com>
2020-10-06 14:39:15 +0200

    Updated settings secret_key.

--- src/urly_project/settings.py
+++ src/urly_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv(DJANGO_SECRET_KEY, '$bp4m75&^0!**_+5^*8j+pkd^i_avv!hof&j!j)-el$gvm8q27')
+SECRET_KEY = '$bp4m75&^0!**_+5^*8j+pkd^i_avv!hof&j!j)-el$gvm8q27'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
